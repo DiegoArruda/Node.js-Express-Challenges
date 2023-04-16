d3a13faf48fe821ea8a3b2f92be4c894c83f2f53
Diego Arruda <arrudadiego1@gmail.com>
2023-04-16 03:54:12 -0300

    Desafio 1 Completo

--- alunos.js
+++ alunos.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const alunos = [
   { nome: "Alana", matricula: 134096, media: 5 },
   { nome: "Rodrigo", matricula: 674384, media: 9 },
@@ -12,4 +13,51 @@ function filtrarMedia(mediaRequerida) {
   return alunos.filter((aluno) => aluno.media >= mediaRequerida);
 }
 
-module.exports = { alunos, acharNome, filtrarMedia };
+function adicionarAluno(nome, matricula, media, alunos) {
+  if (nome !== undefined && matricula !== undefined && media !== undefined) {
+    alunos.push({ nome: nome, matricula: matricula, media: media });
+    var jsonData = JSON.stringify(alunos);
+    fs.writeFileSync("db.json", jsonData);
+    return { message: `Aluno adicionado com sucesso`, alunos };
+  } else {
+    return { message: "Dados inválidos para matricular" };
+  }
+}
+function deletarAluno(index, alunos) {
+  const alunoEncontrado = alunos[index];
+  if (alunoEncontrado !== undefined) {
+    alunos.splice(index, 1);
+    var jsonData = JSON.stringify(alunos);
+    fs.writeFileSync("db.json", jsonData);
+    return { message: `Aluno excluído com sucesso`, alunos };
+  } else {
+    return { message: "Aluno não encontrado" };
+  }
+}
+
+function atualizarAluno(index, dados, alunos) {
+  const alunoEncontrado = alunos[index];
+  if (alunoEncontrado !== undefined) {
+    const { nome, media } = dados;
+    if (nome !== undefined && media !== undefined) {
+      alunoEncontrado.nome = nome;
+      alunoEncontrado.media = media;
+      var jsonData = JSON.stringify(alunos);
+      fs.writeFileSync("db.json", jsonData);
+      return { message: `Alterações feitas com sucesso`, alunos };
+    } else {
+      throw new Error(`Dados inválidos`);
+    }
+  } else {
+    throw new Error(`Aluno não encontrado`);
+  }
+}
+
+module.exports = {
+  alunos,
+  acharNome,
+  filtrarMedia,
+  adicionarAluno,
+  deletarAluno,
+  atualizarAluno,
+};
